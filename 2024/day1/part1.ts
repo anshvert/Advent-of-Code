e59d021f373e279f19d3547c9b23200d56ff315e
ansh <anshtyagi2222@gmail.com>
2024-12-03 02:40:10 +0530

    2024 day2

--- 2024/day1/part1.ts
+++ 2024/day1/part1.ts
@@ -14,8 +14,8 @@ fs.readFile('./input.txt', 'utf8', (err: unknown, data: string): void => {
         locations1.push(parseInt(locations[0],10))
         locations2.push(parseInt(locations[1],10))
     }
-    locations1.sort((a, b) => a - b)
-    locations2.sort((a, b) => a - b)
+    locations1.sort((a: number, b: number): number => a - b)
+    locations2.sort((a: number, b: number): number => a - b)
     for (let i: number = 0; i < locations1.length; i++) {
         diff += Math.abs(locations1[i] - locations2[i])
     }
---
+++ 2024/day2/part1.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs'
+
+interface SafeLevel {
+    (level: string[]): boolean
+}
+
+fs.readFile('./input.txt', 'utf8', (err: unknown, data: string): void => {
+    if (err) {
+        return console.log(err)
+    }
+    let count: number = 0
+    const rows: string[] = data.split('\n')
+    for (let row of rows) {
+        const level: string[] = row.split(" ")
+        if (safeLevel(level)) {
+            count += 1
+        }
+    }
+    console.log(count)
+})
+
+const safeLevel: SafeLevel = (level: string[]): boolean => {
+    const isIncreasing: boolean = parseInt(level[1],10) - parseInt(level[0],10) >= 0
+    for (let i: number = 1; i < level.length; i++) {
+        const diff: number = parseInt(level[i], 10) - parseInt(level[i-1],10)
+        if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+            return false
+        }
+        if ((isIncreasing && diff < 0) || (!isIncreasing && diff > 0)) {
+            return false
+        }
+    }
+    return true
+}
